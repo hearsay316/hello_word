39649e395dc0bbcff1687ae7b222f43c33236556
hearsay316 <qazwsx2228@163.com>
2021-01-04 21:37:00 +0800

    设置跨域属性

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ mod models;
 
 // extern crate actix_web;
 use crate::models::Status;
-use actix_web::{HttpServer, App, web, Responder};
+use actix_web::{HttpServer, App, web, Responder, http};
 use std::io;
+use actix_cors::Cors;
 
 #[warn(non_snake_case)]
 async fn status() -> impl Responder {
@@ -16,7 +17,16 @@ async fn status() -> impl Responder {
 async fn main() -> io::Result<()> {
     println!("项目启动了在 127.0.0.1:8080端口3");
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(status))
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:8081")
+            .allowed_origin_fn(|origin, _req_head| {
+                origin.as_bytes().ends_with(b".rust-lang.org")
+            })
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+        App::new().wrap(cors).route("/", web::post().to(status))
     })
         .bind("127.0.0.1:8080")?
         .run()
