c6f060874b1af06ba4e089698c19971e18472f58
Administrator <admin@example.com>
2021-01-04 18:04:58 +0800

    创建项目

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+// extern crate actix_web;
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+#[warn(non_snake_case)]
+async fn status()->impl Responder{
+    "{\"status\": \"UP\"}"
+}
+#[warn(non_snake_case)]
+#[actix_web::main]
+async fn main() ->io::Result<()>{
+    println!("项目启动了在 127.0.0.1:8080端口");
+    HttpServer::new(|| {
+        App::new().route("/",web::get().to(status))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
