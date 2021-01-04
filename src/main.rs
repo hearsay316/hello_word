c74d2142cb0a96f5976481723025993e3c96133b
hearsay316 <qazwsx2228@163.com>
2021-01-04 19:10:59 +0800

    测试

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn status()->impl Responder{
 #[warn(non_snake_case)]
 #[actix_web::main]
 async fn main() ->io::Result<()>{
-    println!("项目启动了在 127.0.0.1:8080端口");
+    println!("项目启动了在 127.0.0.1:8080端口3");
     HttpServer::new(|| {
         App::new().route("/",web::get().to(status))
     })
