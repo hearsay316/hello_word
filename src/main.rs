1cbce3cfa48dd001f7f8785a8599a4bbe6ef1827
hearsay316 <hearsay316@gamil.com>
2021-01-05 11:59:20 +0800

    修改了 跨域为 *

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> io::Result<()> {
     println!("项目启动了在 127.0.0.1:8080");
     HttpServer::new(|| {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:8081")
+            .allowed_origin("*")
             .allowed_origin_fn(|origin, _req_head| {
                 origin.as_bytes().ends_with(b".rust-lang.org")
             })
